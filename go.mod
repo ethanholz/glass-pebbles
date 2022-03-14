62f723f5eb1235f0ec3cc59534f4031984324d85
Ethan Holz <iplaybassx2@gmail.com>
2022-03-14 00:29:03 -0600

    Added archiving and age encryption

--- go.mod
+++ go.mod
@@ -4,17 +4,23 @@ go 1.17
 
 require (
 	filippo.io/age v1.0.0 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/crackcomm/go-gitignore v0.0.0-20170627025303-887ab5e44cc3 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/ipfs/go-cid v0.1.0 // indirect
 	github.com/ipfs/go-ipfs-api v0.3.0 // indirect
 	github.com/ipfs/go-ipfs-files v0.1.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.11 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
 	github.com/libp2p/go-flow-metrics v0.0.3 // indirect
 	github.com/libp2p/go-libp2p-core v0.14.0 // indirect
 	github.com/libp2p/go-openssl v0.0.7 // indirect
+	github.com/mholt/archiver/v4 v4.0.0-alpha.5 // indirect
 	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
@@ -25,11 +31,16 @@ require (
 	github.com/multiformats/go-multibase v0.0.3 // indirect
 	github.com/multiformats/go-multihash v0.1.0 // indirect
 	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.12 // indirect
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/whyrusleeping/tar-utils v0.0.0-20201201191210-20a61371de5b // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20220313003712-b769efc7c000 // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
 )
--- main.go
+++ main.go
@@ -4,36 +4,66 @@ import (
 	"fmt"
 	"io"
 	"os"
-
+    "flag"
+    "context"
+    "github.com/mholt/archiver/v4"
 	"filippo.io/age"
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
-func main() {
-    // Input file to be read
-    file, err := os.Open("file.txt")
-    // Output encrypted file
-    f, _ := os.Create("out.age")
-    identity, err := age.GenerateX25519Identity()
+func generate_tar(dir_name, output_name string){
+    files, err := archiver.FilesFromDisk(nil, map[string]string{
+        dir_name: "",
+    })
     if err != nil {
-        fmt.Println("Failed to generate key")
+        fmt.Println(err)
     }
-    publicKey := identity.Recipient().String()
-    fmt.Printf("Public key: %s...\n",publicKey)
-    // recipient, err := age.ParseX25519Recipient(publicKey)
-    // Encrypt file and set for my key to be allowed for decryption
-    w, err := age.Encrypt(f, identity.Recipient())
+    out, err := os.Create(output_name)
+    if err != nil{
+        fmt.Println(err)
+    }
+    defer out.Close()
+    format := archiver.CompressedArchive{
+        Compression: archiver.Gz{},
+        Archival: archiver.Tar{},
+    }
+    err = format.Archive(context.Background(), out, files)
+}
+
+func encrypt_file(input_name, output_name, keyfile string)bool{
+    // Input file to be read
+    file, _ := os.Open(input_name)
+    // Output encrypted file
+    f, _ := os.Create(output_name)
+    keyFile, _ := os.Open(keyfile)
+    identities, err := age.ParseRecipients(keyFile)
+    // Encrypt file and set for keys in my
+    w, err := age.Encrypt(f, identities...)
     if err != nil{
         fmt.Println("Failed to create file")
     }
     if _, err := io.Copy(w, file); err != nil{
         fmt.Println("Unable to copy")
     }
-    file.Close()
+    defer file.Close()
+    defer keyFile.Close()
     if err := w.Close(); err != nil{
         fmt.Println("failed to close")
+        return false
     }
+    return true
+    
+}
 
+func main() {
+    key_in := flag.String("k","key.txt","key file")
+    dir := flag.String("d", "./", "input directory")
+    flag.Parse()
+    temp_name := "out.age"
+    in_name := "out.tar.gz"
+    generate_tar(*dir, in_name)
+    encrypt_file(in_name,temp_name,*key_in)
+    file, err := os.Open(temp_name)
     // Add to IPFS node
     sh := shell.NewShell("localhost:5001")
     cid, err := sh.Add(file)
@@ -41,6 +71,6 @@ func main() {
         fmt.Fprintf(os.Stderr, "error: %s", err)
         os.Exit(1)
     }
-    fmt.Printf("%s", cid)
-    
+    fmt.Printf("IPFS Hash: %s\n", cid)
+    file.Close() 
 }
